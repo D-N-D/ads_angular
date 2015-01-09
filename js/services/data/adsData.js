d6ab50c7233c1f71df1ce13e8f5d1762df197896
tutsana <dimitar.nikolaev.dimitrov@gmail.com>
2015-01-09 04:00:00 +0200

    - added showing adds functionality - still WIP

---
+++ js/services/data/adsData.js
@@ -0,0 +1,35 @@
+app.factory('adsData', ['$resource', 'baseServiceUrl',function ($resource, baseServiceUrl) {
+    var resource = $resource(baseServiceUrl + 'ads:adId', {adId: '@id'},{
+        update: {method: 'PUT'}
+    })
+
+    function getPublicAds() {
+        //pageSize, currentPage, startPage
+        return resource.get();
+    }
+
+    function editAd(ad, adId) {
+        return resource.update({id: adId}, ad)
+    }
+
+    function getAdById(adId) {
+        return resource.get({id: adId})
+    }
+
+    function addAd(ad) {
+        return resource.save(ad)
+    }
+
+    function deleteAd(adId) {
+        return resource.delete({id: adId})
+
+    }
+
+    return {
+        getPublicAds: getPublicAds,
+        edit: editAd,
+        getAdById: getAdById,
+        add: addAd,
+        delete: deleteAd
+    }
+}])
